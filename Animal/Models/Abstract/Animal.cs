4bcbe888f5fef0aae6c62726d8a316d5ef03b750
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-02 18:41:13 -0300

    add: estrutura models

---
+++ Animal/Models/Abstract/Animal.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Animal.Models.Interfaces;
+
+namespace Animal.Models.Abstract
+{
+    public abstract class Animal : IAnimal
+    {
+        public Animal() { }
+        public Animal(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+
+        }
+        private string _nome;
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (value != "")
+                    _nome = value;
+                else
+                    throw new NullReferenceException("Nome não pode ser vazio.");
+            }
+        }
+        private int _idade;
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (value > 0)
+                    _idade = value;
+                else
+                    throw new Exception("Idade não pode ser menor que zero.");
+            }
+        }
+
+        public abstract void EmitirSom();
+        public abstract void Mover();
+    }
+}
